fe49fd6268c26ad146313b626e936f0403ef933c
seba <sansoni.sebas@gmail.com>
2024-05-08 19:13:57 -0300

    first exercice added.

---
+++ ejercicios/ej0/ej0.py
@@ -0,0 +1,109 @@
+import rclpy
+import rclpy.logging
+from  rclpy.node  import Node
+import rclpy.qos
+from std_msgs.msg import String,Bool,Int16,Float32
+import random
+
+
+
+class EJ0(Node):
+    def __init__(self):
+        super().__init__('Desencriptador')
+        self.f = open('archivo','r')
+        self.escondido=self.f.readlines()
+        #print(self.escondido)
+        self.f.close()
+        self.sub_4=self.create_subscription(msg_type=String,topic='paso_3',callback=self.paso_3,qos_profile=rclpy.qos.qos_profile_sensor_data)       
+        self.sub_3=self.create_subscription(Float32,'paso_2',self.paso_2,qos_profile=rclpy.qos.qos_profile_sensor_data)
+        self.sub_2=self.create_subscription(Int16,'paso_1',self.paso_1,qos_profile=rclpy.qos.qos_profile_sensor_data)
+        self.sub_1=self.create_subscription(String,'tu_nombre_paso_0',self.paso_0,qos_profile=rclpy.qos.qos_profile_sensor_data)
+        self.pub = self.create_publisher(String,'respuesta',qos_profile=rclpy.qos.qos_profile_sensor_data) 
+
+        self.timer=self.create_timer(0.1,self.run)
+        self.bandera=False
+        self.msg=String()
+        msg='Iniciando el ejercicio 0. Para comenzar debes publicar en el topic correcto tu nombre en minusculas.'
+        self.get_logger().info(msg)
+        self.msg.data=msg
+                    
+    def run(self):          
+        if self.bandera:
+            self.bandera=False                       
+            self.pub.publish(self.msg)        
+
+    def paso_3(self,data):
+        idx=2
+        self.bandera=True    
+        cc=self.decript(self.escondido[idx][0:-1])
+        dd=data.data
+        if type(dd)==float:
+            dd=round(dd,2)
+        if type(dd)!=str:
+            dd=str(dd)
+
+        pos=cc.find(dd)
+        if pos>-1:
+            self.msg.data='Ejercicio finalizado!. Recuerda que cuando te pregunten tu clave es: '+dd         
+        else:
+            self.msg.data='codigo incorrecto'
+
+    def paso_2(self,data):
+        idx=2
+        self.process_(data,idx)
+
+
+    def paso_1(self,data):
+        idx=1
+        self.process_(data,idx)
+
+
+    def paso_0(self,data):
+        idx=0
+        self.process_(data,idx)
+
+    def process_(self,data,idx):
+        self.bandera=True    
+        cc=self.decript(self.escondido[idx][0:-1])
+        dd=data.data
+        if type(dd)==float:
+            dd=round(dd,2)
+        if type(dd)!=str:
+            dd=str(dd)
+
+        pos=cc.find(dd)
+        if pos>-1:
+            nn=len(dd)
+            txt=cc[pos+nn:].split()
+            self.msg.data=txt[0]            
+        else:
+            self.msg.data='codigo incorrecto'
+
+    def decript(self,data):
+        in_=''
+        for d_ in data:
+            asd=ord(d_)+ord(data[0])-122
+            in_=in_+chr(asd)
+        return in_[1:]
+
+    def encript(self,data):        
+        d__=random.randint(97,122)
+        in_=''        
+        in_=in_+chr(d__)
+        for d_ in data:
+            asd=ord(d_)-ord(in_[0])+122
+            in_=in_+chr(asd)                
+        return in_
+
+        
+    
+
+if __name__=='__main__':
+    rclpy.init()
+    ej=EJ0()
+    rclpy.spin(ej)
+    ej.destroy_node()
+    rclpy.shutdown()
+        
+    
+    
