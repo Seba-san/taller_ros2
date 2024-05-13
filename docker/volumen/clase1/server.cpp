da69cce512303dd4b8489cf9383c15359b8650d5
seba <sansoni.sebas@gmail.com>
2024-05-13 13:48:25 -0300

    delay agregado para simular carga.

--- docker/volumen/clase1/server.cpp
+++ docker/volumen/clase1/server.cpp
@@ -2,6 +2,12 @@
 #include "example_interfaces/srv/add_two_ints.hpp"
 #include <memory>
 
+#include <memory>
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
+
 using std::shared_ptr;
 using AddTwoInts = example_interfaces::srv::AddTwoInts;
 using Logger = rclcpp::Logger;
@@ -10,6 +16,7 @@ using Request = AddTwoInts::Request;
 using Response = AddTwoInts::Response;
 
 void add(const shared_ptr<Request> request, shared_ptr<Response> response) {
+    std::this_thread::sleep_for(5s); // Simula carga computacional
     response->sum = request->a + request->b;
 }
 
@@ -17,7 +24,6 @@ int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     auto node = Node::make_shared("add_two_ints_server");
     auto service = node->create_service<AddTwoInts>("add_two_ints", &add);
-
     rclcpp::spin(node);
     rclcpp::shutdown();
 }
